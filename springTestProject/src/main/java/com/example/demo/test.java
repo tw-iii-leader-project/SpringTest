b753563c8d8a4c2c31cd54d8498606b7aead0476
abitw <abitw@githob.com>
2021-01-17 21:54:26 +0800

    gogogo

--- springTestProject/src/main/java/com/example/demo/test.java
+++ springTestProject/src/main/java/com/example/demo/test.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/qq")
+//@RequestMapping(value = "/qq", method = RequestMethod.GET)
 public class test {
 
 	@GetMapping("/x")
