1a16567a1a43b779b222d36e23328d9762f2ebaf
abitw <abitw@githob.com>
2021-02-06 18:21:36 +0800

    最簡易聊天室網頁清酒demo

---
+++ webSakeTest/src/main/java/ab/websake/WebSakeTestApplication.java
@@ -0,0 +1,13 @@
+package ab.websake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSakeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSakeTestApplication.class, args);
+	}
+
+}
---
+++ webSakeTest/src/test/java/ab/websake/WebSakeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ab.websake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSakeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
